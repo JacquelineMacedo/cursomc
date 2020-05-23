5a6eb6db4131d98a5e1dc7a14cbd69d7bc17c79f
Jacqueline Macedo <jacquelinemacedo.ml@gmail.com>
2020-05-23 18:24:41 -0300

    Criação da operação de instanciação

--- src/main/java/com/cursomc/springboot/CursomcApplication.java
+++ src/main/java/com/cursomc/springboot/CursomcApplication.java
@@ -1,12 +1,30 @@
 package com.cursomc.springboot;
 
+import static java.util.Arrays.asList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cursomc.springboot.domain.Categoria;
+import com.cursomc.springboot.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria("Informatica", null);
+		Categoria cat2 = new Categoria("Escritorio", null);
+
+		categoriaRepository.saveAll(asList(cat1, cat2));
 	}
 }
