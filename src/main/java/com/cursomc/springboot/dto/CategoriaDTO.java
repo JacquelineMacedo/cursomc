ade93df9484dc8a8046f5b332d7467824163b253
Jacqueline Macedo <jacquelinemacedo.ml@gmail.com>
2020-06-12 02:02:46 -0300

    Personalizando o retorno da validação

--- src/main/java/com/cursomc/springboot/dto/CategoriaDTO.java
+++ src/main/java/com/cursomc/springboot/dto/CategoriaDTO.java
@@ -21,7 +21,7 @@ public class CategoriaDTO implements Serializable {
 	private Integer id;
 	
 	@NotEmpty(message="Prenchimento obrigatório")
-	@Length(min=5, max=80, message= "O tamnho deve ser entre 5 e 80 caracteres")
+	@Length(min=5, max=80, message= "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO (Categoria categoria) {
--- src/main/java/com/cursomc/springboot/resources/exception/ResourseExceptionHandler.java
+++ src/main/java/com/cursomc/springboot/resources/exception/ResourseExceptionHandler.java
@@ -8,6 +8,8 @@ import static org.springframework.http.ResponseEntity.status;
 import javax.servlet.http.HttpServlet;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -19,17 +21,22 @@ public class ResourseExceptionHandler {
 
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServlet request) {
-
-		StandardError error = new StandardError(NOT_FOUND.value(), e.getMessage(),
-				System.currentTimeMillis());
+		StandardError error = new StandardError(NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(NOT_FOUND).body(error);
 	}
-	
+
 	@ExceptionHandler(DataIntegrityException.class)
 	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException exception, HttpServlet request) {
+		StandardError error = new StandardError(BAD_REQUEST.value(), exception.getMessage(), currentTimeMillis());
+		return status(BAD_REQUEST).body(error);
+	}
 
-		StandardError error = new StandardError(BAD_REQUEST.value(), exception.getMessage(),
-				currentTimeMillis());
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException exception, HttpServlet request) {
+		ValidationError error = new ValidationError(BAD_REQUEST.value(), "Erro de validação", currentTimeMillis());
+		for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
+			error.addError(fieldError.getField(), fieldError.getDefaultMessage());
+		}
 		return status(BAD_REQUEST).body(error);
 	}
 }
